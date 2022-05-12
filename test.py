2b32afbdb688c5f30be3367808acd2e87797916d
Owen Barnett <owenbarnett@Owens-MacBook-Air.local>
2022-05-12 11:32:33 -0500

    Roles

--- test.py
+++ test.py
@@ -1,19 +1,26 @@
 from parsimonious.grammar import Grammar
 grammar = Grammar(
  """
-    roles = newline spaces "roles:" spaces newline spaces list_of_roles
-    list_of_roles = role*
-    role = number_range? spaces input? spaces ":" spaces role_name? spaces chance?
-    spaces = " "*
-    newline = "\\n"+
-    role_name = "initiator | recipient | symbol | location | subject | bystander | absent"+
+   roles = newline spaces "roles:" spaces newline spaces single_role+
+   single_role = spaces role spaces newline
+   role = number_range? spaces input? spaces role_name? spaces chance?
+   spaces = " "*
+   newline = "\\n"+
+   role_name = ~"[initiator | recipient | symbol | location | subject | bystander | absent]+"
+   number_range = ~"[<0-9>]+" "-" ~"[<0-9>]+"
+   input = "$" ~"[<a-z><A-Z>]+"
+   chance = "[0." ~"[<0-9>]+" "]"
+
 """)
 
 # | <INPUT>: <ROLE_NAME> <CHANCE> | <NUMBER_RANGE> <INPUT>: <ROLE_NAME> 
 
-input = '''
-roles: 
-   test   '''.replace('\t', '')
 
+input = '''
+    roles:
+        $insulter initiator 
+        $insultee recipient
+        0-9999 $observer bystander [0.5]
+'''.replace('\t', '')
 
 print(grammar.parse(input))

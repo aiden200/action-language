44bd7e8ca7f81fad3eeb984fcdbcaa4e7def6554
Aiden Chang <aidenchang@MacBook-Pro-52.local>
2022-05-19 11:40:55 -0500

    merging on thursday week8

--- test.py
+++ test.py
@@ -3,23 +3,23 @@
 from parsimonious.grammar import Grammar
 grammar = Grammar(
  '''
-  action = ("special action" spaces name ":" newline manditory_body) / ("action" spaces name ":" newline manditory_body) 
+  action = (spaces newline "special action" spaces name ":" newline manditory_body) / (spaces newline"action" spaces name ":" newline manditory_body) 
   manditory_body = gloss newline roles newline body? newline? salience newline body? newline?
   body = (preconditions newline body) / (effects newline body) 
 
   test= newline expressions
  
-  gloss = "gloss:" spaces string spaces newline
+  gloss = spaces newline? "gloss:" spaces string spaces newline
   effects = newline spaces "effects:" newline expressions
   roles = newline spaces "roles:" spaces newline spaces single_role+
-  salience = ("salience:" newline expressions) / ("saliences:" newline expressions)
+  salience = (spaces? newline? "salience:" newline expressions) / (spaces? newline? "saliences:" newline expressions)
   preconditions = "preconditions:" newline expressions
   single_role = spaces role spaces newline
   role = number_range? spaces input? spaces role_name? spaces chance?
 
   expression = (field_value / code  / string  / conditional / queue / number)
   expressions = (spaces expression newline?)+
-  field_value = spaces string spaces ":" spaces (code / input) spaces newline?
+  field_value = spaces string spaces? ":" spaces (code / input) spaces newline?
   conditional = (if (else / multilineelse)?) / (multilineif (else / multilineelse)?) / (loop / multilineloop)
   loop = spaces "loop" spaces code spaces "as" spaces input spaces ":" spaces newline? spaces expression spaces newline?
   multilineloop = spaces "loop" spaces code spaces "as" spaces input spaces ":" spaces newline spaces "begin" spaces newline spaces expressions spaces newline? spaces "end" spaces newline? spaces
@@ -40,7 +40,7 @@ grammar = Grammar(
   time_of_day = "time_of_day:" spaces code
   abandon = "abandon:" spaces code
 
-  name = ~'[<a-z><A-Z><0-9>*&()^%@!#@,./;?><:}{- ]'+
+  name = ~'[<a-z><A-Z><0-9>*&()^%@!#@,./;?><-]+'
   spaces = " "*
   newline = "\\n"*
   optional_newline= "\\n"*
@@ -50,7 +50,7 @@ grammar = Grammar(
   input = "$" ~"[<a-z><A-Z>]+"
   chance = "[0." ~"[<0-9>]+" "]"
   code = "{" ~"[<a-z><A-Z><0-9>*&()^%@!#@,./;'?><:\[\]]+" "}"
-  string = (~'"[<a-z><A-Z><0-9>*&()^%@!#@,./;?><:{} ]+"') / (~"'[<a-z><A-Z><0-9>*&()^%@!#@,./;?><:{} ]+'")
+  string = (~'"[<a-z><A-Z><0-9>*&()^%@!$#@,./;?><{} ]+"') / (~"'[<a-z><A-Z><0-9>*&()^%@$!#@,./;?><{} ]+'")
 '''
 )
 #    
@@ -83,6 +83,8 @@ special action contemplate-serial-mistreatment:
         $mistreater absent
     salience:
         $mistreated: 100
-'''
+'''.replace('\t', '')
+
+
 
 print((grammar.parse(input)))

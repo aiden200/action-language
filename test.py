db4d71f9281663dda18c3740b3b79170dbc1f71e
Owen Barnett <owenbarnett@Owens-MacBook-Air.local>
2022-05-19 11:40:44 -0500

    Changes

--- test.py
+++ test.py
@@ -3,22 +3,23 @@
 from parsimonious.grammar import Grammar
 grammar = Grammar(
  '''
-  action = ("special action" spaces name ":" newline manditory_body) / ("action" spaces name ":" newline manditory_body) 
-  manditory_body = gloss newline roles newline body? newline? salience newline body? newline?
+ test= spaces newline spaces manditory_body spaces newline
+  action = (spaces newline? spaces "special action" spaces name ":" newline manditory_body) / (spaces newline? spaces "action" spaces name ":" spaces newline manditory_body) 
+  manditory_body = gloss spaces newline? spaces roles? spaces newline? spaces body? spaces newline? spaces salience spaces newline? spaces body? spaces newline? spaces
   body = (preconditions newline body) / (effects newline body) 
 
-  test= newline expressions
  
-  gloss = "gloss:" spaces string spaces newline
-  effects = newline spaces "effects:" newline expressions
-  roles = newline spaces "roles:" spaces newline spaces single_role+
-  salience = ("salience:" newline expressions) / ("saliences:" newline expressions)
+  gloss = spaces "gloss:" spaces string spaces newline?
+  effects = newline spaces "effects:" spaces newline expressions
+  roles = newline? spaces "roles:" spaces newline spaces single_role+ spaces
+  salience = (spaces "salience:" spaces newline expressions) / (spaces "saliences:" spaces newline expressions)
   preconditions = "preconditions:" newline expressions
-  single_role = spaces role spaces newline
+  single_role = spaces role spaces newline?
   role = number_range? spaces input? spaces role_name? spaces chance?
 
-  expression = (field_value / code  / string  / conditional / queue / number)
-  expressions = (spaces expression newline?)+
+  expression = (field_value / code  / string  / conditional / queue / number / variable)
+  variable = spaces input ":" spaces expression spaces 
+  expressions = (spaces expression spaces newline?)+
   field_value = spaces string spaces ":" spaces (code / input) spaces newline?
   conditional = (if (else / multilineelse)?) / (multilineif (else / multilineelse)?) / (loop / multilineloop)
   loop = spaces "loop" spaces code spaces "as" spaces input spaces ":" spaces newline? spaces expression spaces newline?
@@ -40,17 +41,17 @@ grammar = Grammar(
   time_of_day = "time_of_day:" spaces code
   abandon = "abandon:" spaces code
 
-  name = ~'[<a-z><A-Z><0-9>*&()^%@!#@,./;?><:}{- ]'+
+  name = ~'[<a-z><A-Z><0-9>*&()^%@!#@,./;?><}{-]'+
   spaces = " "*
   newline = "\\n"*
   optional_newline= "\\n"*
   number = ~"[0-9]+"
-  role_name = ~"[initiator / recipient / symbol / location / subject / bystander / absent]+"
+  role_name = ("initiator" / "recipient" / "symbol" / "location" / "subject" / "bystander" / "absent")+
   number_range = ~"[<0-9>]+" "-" ~"[<0-9>]+"
   input = "$" ~"[<a-z><A-Z>]+"
   chance = "[0." ~"[<0-9>]+" "]"
-  code = "{" ~"[<a-z><A-Z><0-9>*&()^%@!#@,./;'?><:\[\]]+" "}"
-  string = (~'"[<a-z><A-Z><0-9>*&()^%@!#@,./;?><:{} ]+"') / (~"'[<a-z><A-Z><0-9>*&()^%@!#@,./;?><:{} ]+'")
+  code = "{" ~"[<a-z><A-Z><0-9>*&()^%@!#$@,./;:'?><=\[\] ]+" "}"
+  string = (~'"[<a-z><A-Z><0-9>*&()^%@!#$@,./;?><{} ]+"') / (~"'[<a-z><A-Z><0-9>*&()^%@!#@,./;?><{} ]+'")
 '''
 )
 #    
@@ -59,30 +60,23 @@ grammar = Grammar(
 
 
 input = '''
+    gloss: "{$insulter.name} makes fun of {$insultee.name}"
     roles:
         $insulter initiator 
         $insultee recipient
         0-9999 $observer bystander [0.5]
+    salience:
+        $insultee: 750
+        $insulter: 200
+        loop {$this.all} as $x:
+            if {$x not in $this.nucleus}:
+                if {$x.salience($insultee) >= 1.5}:
+                    500
+                else:
+                    100
+
+
 '''.replace('\t', '')
 
-input = """
-'test'
-"test"
-{tes't's}
-"""
-
-input = '''
-gloss: '{traveler.name} decides to go to {destination.name}'
-'''
-
-input = '''
-special action contemplate-serial-mistreatment:
-    gloss: "{$mistreated.name} contemplate serial mistreatment by {$mistreater.name}"
-    roles:
-        $mistreated initiator
-        $mistreater absent
-    salience:
-        $mistreated: 100
-'''
 
 print((grammar.parse(input)))
